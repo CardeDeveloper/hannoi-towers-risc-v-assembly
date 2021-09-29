dc527449c224434d49976124a36e3e9245e72473
cardedeveloper <oscar.angel.cardenas.a@gmail.com>
2021-09-29 16:41:30 -0500

    avance 2

--- hanoi.asm
+++ hanoi.asm
@@ -1,26 +1,55 @@
-.eqv i t1
+#Oscar Angel cardenas Auilar
+#Marcela Quintero Perez
 
+.eqv i t1
+.eqv discs s0
 .text
 #numero de discos
-addi t0,zero,5
+addi discs,zero,5
 #inicio apuntadores
 lui  s1,0x10010 #torre origen
 addi s2,s1,4#Torre aux
 ori s3,s1,8#Torre destino
 
 #colocamos disco en torre origen
-addi i,zero,1 # iterador
+add i,zero,discs # iterador
 add sp,zero,s1
 init:
 sw i,0(sp)
 addi sp,sp,0x20
-addi i,i,1
-bge t0,i,init
+addi i,i,-1
+blt zero,i,init
 
+#parametros de funcion
+addi sp,sp,-0x20
+add a0,zero,discs #n
+add a1,zero,sp #origen
+add a2,zero,s2 #aux
+add a3,zero,s3 #dest
+jal ra, hanoi
+jal ra, end
 
-
-casoBase:
 
 hanoi:
+#todo sp
+#comprobamos caso base
+addi s10,zero,1
+beq a0,s10,casoBase
+#else
+#hanoi(n-1,o,a,d)
+#a.push(o.pop())
+lw t5,0(a1)
+sw zero,0(a1)
+sw t5,0(a3)
+
+#hanoi(n-1,a,d,o)
+
+
+
+#caso base
+casoBase:
+#mover cuando solo hay 1 disco a la torre destino
+
+
 
 end:
