7dcb06ea8c6f6056d4561f7dd4af9fe69a5ec671
cardedeveloper <oscar.angel.cardenas.a@gmail.com>
2021-09-29 12:39:08 -0500

    initilization

--- hanoi.asm
+++ hanoi.asm
@@ -1,18 +1,23 @@
+.eqv i t1
+
 .text
 #numero de discos
 addi t0,zero,5
 #inicio apuntadores
 lui  s1,0x10010 #torre origen
+addi s2,s1,4#Torre aux
+ori s3,s1,8#Torre destino
 
-ori s2,s1,36#Torre aux
+#colocamos disco en torre origen
+addi i,zero,1 # iterador
+add sp,zero,s1
+init:
+sw i,0(sp)
+addi sp,sp,0x20
+addi i,i,1
+bge t0,i,init
 
-ori s3,s2,64#Torre destino
 
-#addi i,zero,0# iterador
-addi t1,zero,5
-sw t1, 0(s1) 
-sw t1, 0(s2) 
-sw t1, 0(s3) 
 
 casoBase:
 
